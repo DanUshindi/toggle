8299ee3f4cdf2ecc2702c04a054ba7bc50b45bd6
DanUshindi <danushindi5@gmail.com>
2022-02-06 17:17:18 +0200

    dfgu

--- data.js
+++ data.js
@@ -1,7 +1,13 @@
 let hamburger=document.querySelector(".nav-toggle");
 let menu=document.querySelector(".nav-item")
 
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active");
+hamburger.addEventListener("click",(e)=>{
+    hamburger.classList.toggle("active")
     menu.classList.toggle("active")
+    console.log(e.target)
 })
+
+menu.addEventListener("mouseup",(e) => {
+    menu.classList.remove("active")
+    console.log(e.target)
+});
